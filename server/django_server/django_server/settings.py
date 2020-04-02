73b89920b3e1edada5e1c2b6c90d16cfa127e813
jmysliv <jjmreal34@gmail.com>
2020-04-02 13:48:19 +0200

    add requirements

--- server/django_server/django_server/settings.py
+++ server/django_server/django_server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ky6c=x4h=#z^j(!=8y6oi2^r_u(43=*5npcg=72q-y5zk#uw!o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
--- server/django_server/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\jjmre\Desktop\4 semestr\VoiceAssistant\server\django_server\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
