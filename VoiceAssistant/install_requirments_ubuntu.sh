58e5b42a8608eb50404dc130d74fc94759fe9eed
Sebastian <sebastiano139@gmail.com>
2020-04-22 11:33:57 +0200

    Add libespeak1 library to script installation

--- VoiceAssistant/install_requirments_ubuntu.sh
+++ VoiceAssistant/install_requirments_ubuntu.sh
@@ -3,6 +3,7 @@ cd VoiceAssistant
 python3 -m venv venv
 source venv/bin/activate
 pip install wheel
+sudo apt install libespeak1
 sudo apt-get install libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0
 sudo apt-get install ffmpeg libav-tools
 sudo apt-get update & sudo apt-get install espeak & sudo apt-get install python3-tk
